dd88fbc6a17832a999f82c82a1ac965ed52b489f
Kamil Urbanik <kamil.urbanik@miggroup.com>
2024-04-19 12:57:36 +0200

    #0009 - end project, add Log4j to file, add README.md

---
+++ call-app/src/main/java/pl/urbanik/callapp/exception/ApplicationException.java
@@ -0,0 +1,11 @@
+package pl.urbanik.callapp.exception;
+
+/**
+ * @author kurbanik
+ */
+
+public class ApplicationException extends Exception {
+    public ApplicationException(String message) {
+        super(message);
+    }
+}
